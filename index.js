6fb66099272dc57eac7a0fc728175571fba592a8
Matt Krause <krause.matt@pm.me>
2022-05-19 22:41:02 -0400

    Connected MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
 
+dotenv.config();
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB);
+    console.log("Travelsite connected to MongoDB");
+  } catch (error) {
+    console.log("MongoDB connection error");
+    throw error;
+  }
+};
+
 app.listen(port, () => {
   console.log(`Travelsite listening on port ${port}`);
+  dbConnect();
 });
