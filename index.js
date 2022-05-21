9563b89c9f44cc3093386997c9001f23086bfeac
Matt Krause <krause.matt@pm.me>
2022-05-20 22:37:15 -0400

    Starting mongoose schemas and establishing CRUD operations for routes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
 
 const app = express();
 const port = 3000;
@@ -18,7 +21,11 @@ const dbConnect = async () => {
   }
 };
 
+app.use(express.json());
 app.use("/auth", authRoute);
+app.use("/hotels", hotelsRoute);
+app.use("/rooms", roomsRoute);
+app.use("/users", usersRoute);
 
 app.listen(port, () => {
   console.log(`Travelsite listening on port ${port}`);
