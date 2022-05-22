ce4eb7b8c49601da2fadde422b0d3a081c3d457f
Matt Krause <krause.matt@pm.me>
2022-05-21 20:22:36 -0400

    Completed RESTful API logic for hotel routes

--- routes/hotels.js
+++ routes/hotels.js
@@ -3,12 +3,68 @@ import Hotel from "../models/Hotels.js";
 
 const router = express.Router();
 
+// CREATE
 router.post("/", async (req, res) => {
   const newHotel = new Hotel(req.body);
-
   try {
     const savedHotel = await newHotel.save();
     res.status(200).json(savedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// READ (All)
+router.get("/all", async (req, res) => {
+  try {
+    const findAllHotels = await Hotel.find({});
+    res.status(200).json(findAllHotels);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// READ (Single)
+router.get("/:id", async (req, res) => {
+  try {
+    const findHotel = await Hotel.findById({ _id: req.params.id });
+    res.status(200).json(findHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE (All)
+router.delete("/all", async (req, res) => {
+  try {
+    const deleteAllHotels = await Hotel.deleteMany({});
+    res.status(200).json(deleteAllHotels);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE (Single)
+router.delete("/:id", async (req, res) => {
+  try {
+    const deleteHotel = await Hotel.findByIdAndDelete({ _id: req.params.id });
+    res.status(200).json(deleteHotel);
   } catch (err) {
     res.status(500).json(err);
   }
