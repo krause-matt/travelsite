5bad8f60bc2379a56307c322803e068d0e1c4d5e
Matt Krause <krause.matt@pm.me>
2022-05-26 23:19:17 -0400

    Finished middleware for authentication and authorization

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,8 +6,25 @@ import {
   deleteAllUsers,
   deleteUser,
 } from "../controllers/users.js";
+import {
+  checkAdmin,
+  checkAuthentication,
+  checkAuthorization,
+} from "../utils/tokenAuth.js";
 
 const router = express.Router();
+
+router.get("/authenticate", checkAuthentication, (req, res, next) => {
+  res.send("Hello user");
+});
+
+router.get("/auth/:id", checkAuthorization, (req, res, next) => {
+  res.send("Hello user, you have editing priveledges");
+});
+
+router.get("/admin/:id", checkAdmin, (req, res, next) => {
+  res.send("Hello user, you have admin priveledges");
+});
 
 // READ (All)
 router.get("/all", getAllUsers);
--- api/utils/tokenAuth.js
+++ api/utils/tokenAuth.js
@@ -1,2 +1,34 @@
 import jwt from "jsonwebtoken";
 import { errorHandler } from "./errorHandler.js";
+
+export const checkAuthentication = (req, res, next) => {
+  const token = req.cookies.auth_token;
+  if (!token)
+    return next(errorHandler(401, "You do not have an access token."));
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (error, userInfo) => {
+    if (error) return next(errorHandler(403, "Invalid access token."));
+    req.user = userInfo;
+    next();
+  });
+};
+
+export const checkAuthorization = (req, res, next) => {
+  checkAuthentication(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(errorHandler(403, "You do not have priviledges to edit."));
+    }
+  });
+};
+
+export const checkAdmin = (req, res, next) => {
+  checkAuthentication(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      return next(errorHandler(403, "You do not have admin priviledges."));
+    }
+  });
+};
