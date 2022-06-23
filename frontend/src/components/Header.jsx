97fda2ea0e38f7cf4692a100ba62d16b3fdfd7d9
Matt Krause <krause.matt@pm.me>
2022-06-22 23:11:35 -0400

    Working on header search elements

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import "./header.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faCalendarAlt,
   faCarSide,
   faHotel,
   faParachuteBox,
+  faPerson,
   faPlaneDeparture,
   faTaxi,
 } from "@fortawesome/free-solid-svg-icons";
@@ -14,26 +16,44 @@ export const Header = () => {
     <div className="header">
       <div className="container">
         <div className="list">
-          <div className="list-item">
+          <div className="list-item active">
             <FontAwesomeIcon icon={faHotel} />
-            <span className="description">Hotels</span>
+            <span className="item-description">Hotels</span>
           </div>
           <div className="list-item">
             <FontAwesomeIcon icon={faPlaneDeparture} />
-            <span className="description">Flights</span>
+            <span className="item-description">Flights</span>
           </div>
           <div className="list-item">
             <FontAwesomeIcon icon={faCarSide} />
-            <span className="description">Cars</span>
+            <span className="item-description">Cars</span>
           </div>
           <div className="list-item">
             <FontAwesomeIcon icon={faParachuteBox} />
-            <span className="description">Entertainment</span>
+            <span className="item-description">Entertainment</span>
           </div>
           <div className="list-item">
             <FontAwesomeIcon icon={faTaxi} />
-            <span className="description">Taxi</span>
+            <span className="item-description">Taxi</span>
           </div>
+        </div>
+        <h1 className="title">Travelsite Title</h1>
+        <p className="site-description">Travelsite description</p>
+        <button className="header-button">Sign In</button>
+        <div className="search">
+          <div className="search-item">
+            <FontAwesomeIcon icon={faHotel} />
+            <input type="text" placeholder="Destination" />
+          </div>
+          <div className="search-item">
+            <FontAwesomeIcon icon={faCalendarAlt} />
+            <span>Date range</span>
+          </div>
+          <div className="search-item">
+            <FontAwesomeIcon icon={faPerson} />
+            <span>Adults + children</span>
+          </div>
+          <button className="search-button">Search</button>
         </div>
       </div>
     </div>
