d7e05cdf0e42866d0f566643643ee58df58d67df
Matt Krause <krause.matt@pm.me>
2022-06-29 23:18:27 -0400

    Starting on search result hotel list page

--- frontend/src/webpages/HotelList.jsx
+++ frontend/src/webpages/HotelList.jsx
@@ -8,6 +8,18 @@ export const HotelList = () => {
     <>
       <NavBar />
       <Header display="partial" />
+      <div className="hotel-list">
+        <div className="list-container">
+          <div className="list-search">
+            <h1 className="search-title">Search</h1>
+            <div className="search-group">
+              <span>Destination</span>
+              <input type="text" placeholder="text"></input>
+            </div>
+          </div>
+          <div className="list-results"></div>
+        </div>
+      </div>
     </>
   );
 };
